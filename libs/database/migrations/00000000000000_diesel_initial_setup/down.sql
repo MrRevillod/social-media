4f3a3c9537c8ac5cb9a64bcf767d397f6b0ff1fb
MrRevillod <lrevillod2022@alu.uct.cl>
2025-02-13 00:41:59 -0300

    feat: :sparkles: Add prisma orm project support

--- libs/database/migrations/00000000000000_diesel_initial_setup/down.sql
+++
@@ -1,6 +0,0 @@
--- This file was automatically created by Diesel to setup helper functions
--- and other internal bookkeeping. This file is safe to edit, any future
--- changes will be added to existing projects as new migrations.
-
-DROP FUNCTION diesel_manage_updated_at(regclass);
-DROP FUNCTION diesel_set_updated_at();
--- libs/database/migrations/2025-02-11-023458_users_and_sessions/down.sql
+++
@@ -1,6 +0,0 @@
--- This file should undo anything in `up.sql`
-
-DROP TABLE sessions;
-DROP TABLE users;
-DROP FUNCTION diesel_manage_updated_at(regclass);
-DROP FUNCTION diesel_set_updated_at();
